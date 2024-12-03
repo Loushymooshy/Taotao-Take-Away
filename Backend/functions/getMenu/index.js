137a03bc705bea261a2da96c5c200e8646f3c33e
Vendelin2up <amy_pamy90@hotmail.com>
2024-12-03 11:22:50 +0100

    glömde stoppa in middlewaren i alla filer

--- Backend/functions/getMenu/index.js
+++ Backend/functions/getMenu/index.js
@@ -1,5 +1,6 @@
 const { sendResponse, sendError } = require("../../responses/index");
 const { db } = require("../../services/db");
+const { checkApiKey } = require("../../middleware/checkApiKey");
 
 async function getMenu() {
   const { Items } = await db.scan({
@@ -10,6 +11,11 @@ async function getMenu() {
 }
 
 exports.handler = async (event) => {
+  // Kontrollera API-nyckeln
+  const apiKeyError = checkApiKey(event);
+  if (apiKeyError) {
+    return apiKeyError; // Returnera fel om nyckeln inte är giltig
+  }
   try {
     const data = await getMenu();
     return sendResponse(data);
