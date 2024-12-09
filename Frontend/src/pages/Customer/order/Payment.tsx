3bf395b5c8228f8f664d885de0445b48dcd31923
Baranbulduk <baranbulduk99@gmail.com>
2024-12-09 10:55:11 +0100

    skapat en betalningsvy

--- Frontend/src/pages/Customer/order/Payment.tsx
+++ Frontend/src/pages/Customer/order/Payment.tsx
@@ -1,29 +1,93 @@
 const Payment: React.FC = () => {
   return (
     <>
-      <h1>PAYMENT</h1>
-      <form>
-        <section>
-          <label htmlFor="card-owmer">CARD OWNER:</label>
-          <input type="text" id="card-owmer" name="card-owmer" />
-          <label htmlFor="card-number">CARD NUMBER:</label>
-          <input type="text" id="card-number" name="card-number" />
-          <label htmlFor="validity-period">VALIDITY PERIOD:</label>
-          <input type="text" id="validity-period" name="validity-period" />
-          <label htmlFor="cvc">CVC:</label>
-          <input type="text" id="cvc" name="cvc" />
-          <button type="submit">PAY WITH CARD</button>
-        </section>
-        <p>OR</p>
-        <section>
-          <label htmlFor="swish-number">SWISH NUMBER:</label>
-          <input type="text" id="swish-number" name="swish-number" />
-          <button type="submit">PAY WITH SWISH</button>
-        </section>
-        <section>
-          <button type="submit">CANCEL PAYMENT</button>
-        </section>
-      </form>
+      <main>
+        <h1 className="flex justify-center items-center text-4xl font-black h-48 font-Darumadrop">
+          PAYMENT
+        </h1>
+        <div className="flex flex-row gap-60">
+          <section>
+            <h2>YOUR ORDERS</h2>
+            <p>Lorem Ipsum</p>
+            <h2>TOTAL: $100</h2>
+          </section>
+          <form className="bg-pandaWhite text-pandaBlack flex flex-col p-6 w-70 rounded-md">
+            <label className="mt-2 font-bold" htmlFor="card-owmer">
+              CARD OWNER:
+            </label>
+            <input
+              className="rounded-md h-8 px-2"
+              type="text"
+              id="card-owmer"
+              name="card-owmer"
+            />
+            <label className="mt-2 font-bold" htmlFor="card-number">
+              CARD NUMBER:
+            </label>
+            <input
+              className="rounded-md h-8 px-2"
+              type="text"
+              id="card-number"
+              name="card-number"
+            />
+            <div className="flex flex-row gap-5">
+              <div className="flex flex-col">
+                <label className="mt-2 font-bold" htmlFor="validity-period">
+                  VALIDITY PERIOD:
+                </label>
+                <input
+                  className="rounded-md h-8 px-2"
+                  type="text"
+                  id="validity-period"
+                  name="validity-period"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="mt-2 font-bold" htmlFor="cvc">
+                  CVC:
+                </label>
+                <input
+                  className="rounded-md h-8 px-2"
+                  type="text"
+                  id="cvc"
+                  name="cvc"
+                />
+              </div>
+            </div>
+            <button
+              className="bg-themeGreen text-pandaWhite rounded-md mt-4 mb-2 h-12"
+              type="submit"
+            >
+              PAY WITH CARD
+            </button>
+            <p className="flex justify-center items-center h-20 font-bold">
+              OR
+            </p>
+            <label className="mt-2 font-bold" htmlFor="swish-number">
+              SWISH NUMBER:
+            </label>
+            <input
+              className="rounded-md h-8 px-2"
+              type="text"
+              id="swish-number"
+              name="swish-number"
+            />
+            <button
+              className="bg-themeGreen text-pandaWhite rounded-md mt-4 mb-2 h-12"
+              type="submit"
+            >
+              PAY WITH SWISH
+            </button>
+            <button
+              className="bg-themeRed
+          text-pandaWhite rounded-md mt-10 mb-2 h-12"
+              type="submit"
+            >
+              CANCEL PAYMENT
+            </button>
+          </form>
+        </div>
+      </main>
     </>
   );
 };
